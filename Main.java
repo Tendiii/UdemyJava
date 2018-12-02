94122738221975143431b76ef0011c09fb6fd337
Tendiii <44947923+Tendiii@users.noreply.github.com>
2018-12-02 18:55:41 +0100

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,10 @@
+package pl.LukaszTendaj.fistrprogram;
+/* komentarz na
+     pare
+     linijek tekstu */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("lololo");
+    }
+    //zwykły komentarz
+}
